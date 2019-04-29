45b4a60c793ac6cd080e4b971e6ea811978068a1
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-29 16:31:49 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,6 @@
+sentence1 = "Hello, there and how are you?"
+
+def reverse_each_word(sentence1)
+  sentence1.split.map {|word| word.reverse}.join(" ")
+  sentence1.split.collect {|word| word.reverse}.join(" ")
+end
